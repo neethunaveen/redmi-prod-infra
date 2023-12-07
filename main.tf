2dad2953e178ff1e4fce1246b50dafb509839264
neethu <neethusnayar@gmail.com>
2023-12-07 06:55:53 +0000

    keypair creation

---
+++ main.tf
@@ -0,0 +1,16 @@
+# ------------------------------------------------
+# Key pair creation
+# ------------------------------------------------
+
+resource "aws_key_pair" "auth_key" {
+
+  key_name   = "${var.project_name}-${var.project_env}"
+  public_key = file("mykey.pub")
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+}
