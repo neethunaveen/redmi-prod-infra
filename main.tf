36a29c17c7b05ae6f0f546c7fe78af1710b4f2f7
neethu <neethusnayar@gmail.com>
2023-12-07 07:00:33 +0000

    web-access sg created

--- main.tf
+++ main.tf
@@ -14,3 +14,37 @@ resource "aws_key_pair" "auth_key" {
     owner   = var.project_owner
   }
 }
+resource "aws_security_group" "http-access" {
+  name        = "${var.project_name}-${var.project_env}-http-access"
+  description = "${var.project_name}-${var.project_env}-http-access"
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name    = "${var.project_name}-${var.project_env}-http-access"
+    project = var.project_name
+    env     = var.project_env
+    owner   = var.project_owner
+  }
+}
